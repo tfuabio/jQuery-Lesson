86cf6305d55b4bce27a90c2f33aac26eeb4f722e
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-12-04 07:46:42 +0000

    [finish] 確認問題

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(function() {
-    $('.box1').slideDown(3000);
-    // $('.box1').slideUp(3000);
-    // alert('OK!');
-    // $('.box1').show(3000);
-    // $('.box1').css({'background-color': '#0000FF'})
-    // $('.box1').hide(3000);
-    // $('.box1').fadeOut(3000);
+    $('.box1').slideDown(3000, function(){
+      $('.box1').css({
+        'background-color': '#0000FF',
+        'height': '100px',
+        'weight': '200px'
+      }).slideUp(3000);
+    });
 });

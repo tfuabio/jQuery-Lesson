3c488c863ea18dc78be63103fb40c0866bd21340
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-12-04 07:08:05 +0000

    [Fix] 非表示の要素を表示

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-$(function(){
-    // $('.box1').slideDown();
+$(function() {
+    $('.box1').slideDown(3000);
+    // $('.box1').slideUp(3000);
     // alert('OK!');
-    // $('.box1').show();
+    // $('.box1').show(3000);
     // $('.box1').css({'background-color': '#0000FF'})
-    $('.box1').hide();
+    // $('.box1').hide(3000);
+    // $('.box1').fadeOut(3000);
 });
